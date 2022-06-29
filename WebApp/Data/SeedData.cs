aad018589e745197a290036f486212e5f32adf66
Rafael Pereira Dias <br.rafaeldias@gmail.com>
2022-06-29 18:41:58 -0300

    changed place where seeddata file was

---
+++ WebApp/Data/SeedData.cs
@@ -0,0 +1,103 @@
+﻿using WebApp.Data.Shared;
+using WebApp.Models.Machines;
+using WebApp.Models.Products;
+
+namespace WebApp.Data
+{
+    public class SeedData
+    {
+        public void Initialize()
+        {
+            List<Machine> list = new List<Machine>()
+            {
+                new Machine()
+                {
+                    MachineId = 0,
+                    MachineName = "Coke Machine",
+                    MachineBalance = 0,
+                    MachineProductList = new List<Product>
+                    {
+                        new Product()
+                        {
+                            ProductId = 0,
+                            ProductName = "Coke can",
+                            ProductPrice = 2.5m,
+                            ProductQuantity = 10
+                        },
+                        new Product()
+                        {
+                            ProductId = 1,
+                            ProductName = "Water bottle",
+                            ProductPrice = 1.0m,
+                            ProductQuantity = 10
+                        },
+
+                    }
+                },
+                new Machine()
+                {
+                    MachineId = 1,
+                    MachineName = "Pepsi Machine",
+                    MachineBalance = 50m,
+                    MachineProductList = new List<Product>
+                    {
+                        new Product()
+                        {
+                            ProductId = 0,
+                            ProductName = "Pepsi can",
+                            ProductPrice = 2.55m,
+                            ProductQuantity = 10
+                        }
+                    }
+                }
+            };
+            SharedData.Current = list[0];
+        }
+        public static List<Machine> ListView()
+        {
+            return new List<Machine>()
+            {
+                new Machine()
+                {
+                    MachineId = 0,
+                    MachineName = "Coke Machine",
+                    MachineBalance = 0,
+                    MachineProductList = new List<Product>
+                    {
+                        new Product()
+                        {
+                            ProductId = 0,
+                            ProductName = "Coke can",
+                            ProductPrice = 2.5m,
+                            ProductQuantity = 10
+                        },
+                        new Product()
+                        {
+                            ProductId = 1,
+                            ProductName = "Water bottle",
+                            ProductPrice = 1.0m,
+                            ProductQuantity = 10
+                        },
+
+                    }
+                },
+                new Machine()
+                {
+                    MachineId = 1,
+                    MachineName = "Pepsi Machine",
+                    MachineBalance = 50m,
+                    MachineProductList = new List<Product>
+                    {
+                        new Product()
+                        {
+                            ProductId = 0,
+                            ProductName = "Pepsi can",
+                            ProductPrice = 2.55m,
+                            ProductQuantity = 10
+                        }
+                    }
+                }
+            };
+        }
+    }
+}
