69eaf24f1da2a11cd4da94adad8604a578ba9411
Rafael Pereira Dias <br.rafaeldias@gmail.com>
2022-06-30 00:04:43 -0300

    using shareddata to access data throught my project

--- WebApp/Data/SeedData.cs
+++ WebApp/Data/SeedData.cs
@@ -6,98 +6,55 @@ namespace WebApp.Data
 {
     public class SeedData
     {
-        public void Initialize()
+        public static void Initialize()
         {
-            List<Machine> list = new List<Machine>()
-            {
-                new Machine()
-                {
-                    MachineId = 0,
-                    MachineName = "Coke Machine",
-                    MachineBalance = 0,
-                    MachineProductList = new List<Product>
-                    {
-                        new Product()
-                        {
-                            ProductId = 0,
-                            ProductName = "Coke can",
-                            ProductPrice = 2.5m,
-                            ProductQuantity = 10
-                        },
-                        new Product()
-                        {
-                            ProductId = 1,
-                            ProductName = "Water bottle",
-                            ProductPrice = 1.0m,
-                            ProductQuantity = 10
-                        },
+            //List<Machine> list = new List<Machine>()
+            //{
+            //    new Machine()
+            //    {
+            //        MachineId = 0,
+            //        MachineName = "Coke Machine",
+            //        MachineBalance = 0,
+            //        MachineProductList = new List<Product>
+            //        {
+            //            new Product()
+            //            {
+            //                ProductId = 0,
+            //                ProductName = "Coke can",
+            //                ProductPrice = 2.5m,
+            //                ProductQuantity = 10
+            //            },
+            //            new Product()
+            //            {
+            //                ProductId = 1,
+            //                ProductName = "Water bottle",
+            //                ProductPrice = 1.0m,
+            //                ProductQuantity = 10
+            //            },
 
-                    }
-                },
-                new Machine()
-                {
-                    MachineId = 1,
-                    MachineName = "Pepsi Machine",
-                    MachineBalance = 50m,
-                    MachineProductList = new List<Product>
-                    {
-                        new Product()
-                        {
-                            ProductId = 0,
-                            ProductName = "Pepsi can",
-                            ProductPrice = 2.55m,
-                            ProductQuantity = 10
-                        }
-                    }
-                }
-            };
-            SharedData.Current = list[0];
-        }
-        public static List<Machine> ListView()
-        {
-            return new List<Machine>()
-            {
-                new Machine()
-                {
-                    MachineId = 0,
-                    MachineName = "Coke Machine",
-                    MachineBalance = 0,
-                    MachineProductList = new List<Product>
-                    {
-                        new Product()
-                        {
-                            ProductId = 0,
-                            ProductName = "Coke can",
-                            ProductPrice = 2.5m,
-                            ProductQuantity = 10
-                        },
-                        new Product()
-                        {
-                            ProductId = 1,
-                            ProductName = "Water bottle",
-                            ProductPrice = 1.0m,
-                            ProductQuantity = 10
-                        },
-
-                    }
-                },
-                new Machine()
-                {
-                    MachineId = 1,
-                    MachineName = "Pepsi Machine",
-                    MachineBalance = 50m,
-                    MachineProductList = new List<Product>
-                    {
-                        new Product()
-                        {
-                            ProductId = 0,
-                            ProductName = "Pepsi can",
-                            ProductPrice = 2.55m,
-                            ProductQuantity = 10
-                        }
-                    }
-                }
-            };
+            //        }
+            //    },
+            //    new Machine()
+            //    {
+            //        MachineId = 1,
+            //        MachineName = "Pepsi Machine",
+            //        MachineBalance = 50m,
+            //        MachineProductList = new List<Product>
+            //        {
+            //            new Product()
+            //            {
+            //                ProductId = 0,
+            //                ProductName = "Pepsi can",
+            //                ProductPrice = 2.55m,
+            //                ProductQuantity = 10
+            //            }
+            //        }
+            //    }
+            //};
+            List<Machine> machineList = new List<Machine> { };
+            List<Product> productList = new List<Product> { };
+            SharedData.MachineList = machineList;
+            SharedData.ProductList = productList;
         }
     }
 }
---
+++ WebApp/Data/Shared/SharedData.cs
@@ -0,0 +1,11 @@
+﻿using WebApp.Models.Machines;
+using WebApp.Models.Products;
+
+namespace WebApp.Data.Shared
+{
+    public class SharedData
+    {
+        public static List<Machine> MachineList { get; set; } = default!;
+        public static List<Product> ProductList { get; set; } = default!;
+    }
+}
