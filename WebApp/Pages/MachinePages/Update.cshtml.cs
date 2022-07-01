908b2cf0b3092f226b79f40dc48ca84fe6661413
Rafael Pereira Dias <br.rafaeldias@gmail.com>
2022-07-01 01:02:17 -0300

    Update from crud works with machine changes, but not with products. Guid from machine and products change every post.

--- WebApp/Pages/MachinePages/Update.cshtml.cs
+++ WebApp/Pages/MachinePages/Update.cshtml.cs
@@ -10,16 +10,19 @@ namespace WebApp.Pages.MachinePages
         private readonly List<Machine> Machines = SharedData.MachineList;
 
         [BindProperty(SupportsGet = true)]
+        public Guid Id { get; set; }
+
+        [BindProperty(SupportsGet = true)]
         public Machine FormMachine { get; set; }
 
-        public IActionResult OnGet(Guid? id)
+        public IActionResult OnGet(Guid? Id)
         {
-            if (id == null || Machines == null)
+            if (Id == null || Machines == null)
             {
                 return Redirect("../List");
             }
 
-            var machineExist = Machines.FirstOrDefault(m => m.MachineId.Equals(id));
+            var machineExist = Machines.FirstOrDefault(m => m.MachineId.Equals(Id));
             if (machineExist == null)
             {
                 return Redirect("../List");
@@ -31,6 +34,11 @@ namespace WebApp.Pages.MachinePages
 
         public IActionResult OnPost()
         {
+            // TODO Solve problem with Guid from machine and product changing after each post.
+            SharedData.MachineList.First(m => m.MachineId.Equals(Id)).MachineName = FormMachine.MachineName;
+            SharedData.MachineList.First(m => m.MachineId.Equals(Id)).MachineBalance = FormMachine.MachineBalance;
+            SharedData.MachineList.First(m => m.MachineId.Equals(Id)).MachineProductList = FormMachine.MachineProductList;
+            SharedData.ProductList.AddRange(FormMachine.MachineProductList);
             return RedirectToPage("/MachinePages/List");
         }
     }
