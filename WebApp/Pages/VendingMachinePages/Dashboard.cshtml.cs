152fdac498d3ce219a11b3193eb1b92b83152974
Rafael Pereira Dias <br.rafaeldias@gmail.com>
2022-07-01 20:11:38 -0300

    created dashboard razor page and added an anchor at navbar _Layout to dashboard

---
+++ WebApp/Pages/VendingMachinePages/Dashboard.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace WebApp.Pages.VendingMachinePages
+{
+    public class DashboardModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
