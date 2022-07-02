7e341da55f37a003eb9b44b638e9f6f46c5da34e
Rafael Pereira Dias <br.rafaeldias@gmail.com>
2022-07-01 22:39:47 -0300

    created left menu and main content. Add partials to render at get page.

--- WebApp/Pages/VendingMachinePages/Dashboard.cshtml.cs
+++ WebApp/Pages/VendingMachinePages/Dashboard.cshtml.cs
@@ -3,6 +3,11 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace WebApp.Pages.VendingMachinePages
 {
+    // FEATURE Dashboard creation.
+    // TODO Dashboard left menu. Machines + Products + Reports. Button to main menu/refresh page.
+    // TODO Dashboard main tag. 1 main container with 4 containers. Each container will be list for machine, product, reports and revenue?
+    // TODO Dashboard left menu buttoms. Each buttom will update main view for respective area.
+
     public class DashboardModel : PageModel
     {
         public void OnGet()
