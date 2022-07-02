8674330a228b919dcc142223806cb99be7fbb405
Rafael Pereira Dias <br.rafaeldias@gmail.com>
2022-07-02 03:31:13 -0300

    seed data populating an list with machines and products.

--- WebApp/Data/SeedData.cs
+++ WebApp/Data/SeedData.cs
@@ -8,50 +8,49 @@ namespace WebApp.Data
     {
         public static void Initialize()
         {
-            //List<Machine> list = new List<Machine>()
-            //{
-            //    new Machine()
-            //    {
-            //        MachineId = 0,
-            //        MachineName = "Coke Machine",
-            //        MachineBalance = 0,
-            //        MachineProductList = new List<Product>
-            //        {
-            //            new Product()
-            //            {
-            //                ProductId = 0,
-            //                ProductName = "Coke can",
-            //                ProductPrice = 2.5m,
-            //                ProductQuantity = 10
-            //            },
-            //            new Product()
-            //            {
-            //                ProductId = 1,
-            //                ProductName = "Water bottle",
-            //                ProductPrice = 1.0m,
-            //                ProductQuantity = 10
-            //            },
-
-            //        }
-            //    },
-            //    new Machine()
-            //    {
-            //        MachineId = 1,
-            //        MachineName = "Pepsi Machine",
-            //        MachineBalance = 50m,
-            //        MachineProductList = new List<Product>
-            //        {
-            //            new Product()
-            //            {
-            //                ProductId = 0,
-            //                ProductName = "Pepsi can",
-            //                ProductPrice = 2.55m,
-            //                ProductQuantity = 10
-            //            }
-            //        }
-            //    }
-            //};
-            List<Machine> machineList = new List<Machine> { };
+            List<Machine> machineList = new List<Machine>()
+            {
+                new Machine()
+                {
+                    MachineId = Guid.NewGuid(),
+                    MachineName = "Coke Machine",
+                    MachineBalance = 0,
+                    MachineProductList = new List<Product>
+                    {
+                        new Product()
+                        {
+                            ProductId = Guid.NewGuid(),
+                            ProductName = "Coke can",
+                            ProductPrice = 2.5m,
+                            ProductQuantity = 10
+                        },
+                        new Product()
+                        {
+                            ProductId = Guid.NewGuid(),
+                            ProductName = "Water bottle",
+                            ProductPrice = 1.0m,
+                            ProductQuantity = 10
+                        },
+                    }
+                },
+                new Machine()
+                {
+                    MachineId = Guid.NewGuid(),
+                    MachineName = "Pepsi Machine",
+                    MachineBalance = 50m,
+                    MachineProductList = new List<Product>
+                    {
+                        new Product()
+                        {
+                            ProductId = Guid.NewGuid(),
+                            ProductName = "Pepsi can",
+                            ProductPrice = 2.55m,
+                            ProductQuantity = 10
+                        }
+                    }
+                }
+            };
+            //List<Machine> machineList = new List<Machine> { };
             List<Product> productList = new List<Product> { };
             SharedData.MachineList = machineList;
             SharedData.ProductList = productList;
