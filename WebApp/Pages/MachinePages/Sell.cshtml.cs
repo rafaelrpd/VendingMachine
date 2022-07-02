83f125d144ac5fb4bc5724ebeb9ead76689d38fa
Rafael Pereira Dias <br.rafaeldias@gmail.com>
2022-07-01 22:50:07 -0300

    create sell razor page and update layout to link it.

---
+++ WebApp/Pages/MachinePages/Sell.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace WebApp.Pages.MachinePages
+{
+    public class SellModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
