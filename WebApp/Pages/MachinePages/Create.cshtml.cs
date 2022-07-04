dd85be8d720533ca6c75a48a9555a9666eb1de5a
Rafael Pereira Dias <br.rafaeldias@gmail.com>
2022-07-04 03:35:19 -0300

    refactored create page to only create  a machine.

--- WebApp/Pages/MachinePages/Create.cshtml.cs
+++ WebApp/Pages/MachinePages/Create.cshtml.cs
@@ -2,7 +2,6 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using WebApp.Data.Shared;
 using WebApp.Models.Machines;
-using WebApp.Models.Products;
 
 namespace WebApp.Pages.MachinePages
 {
@@ -10,19 +9,14 @@ namespace WebApp.Pages.MachinePages
     {
         public void OnGet()
         {
-
         }
 
-        public int ProductCount { get; set; } = 3;
-
         [BindProperty]
-        public Machine FormMachine { get; set; }
-
+        public Machine FormMachine { get; set; } = new Machine();
 
         public RedirectToPageResult OnPost()
         {
             SharedData.MachineList.Add(FormMachine);
-            SharedData.ProductList.AddRange(FormMachine.MachineProductList);
             return RedirectToPage("/MachinePages/List");
         }
     }
