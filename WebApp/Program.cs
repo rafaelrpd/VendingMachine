893774a45330dea96ec07d7702c5af7caed85916
Rafael Pereira Dias <br.rafaeldias@gmail.com>
2024-08-09 03:22:03 -0300

    CRUD webapp basic product page done with tests

--- WebApp/Program.cs
+++ WebApp/Program.cs
@@ -1,4 +1,9 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using WebApp.Data;
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddDbContext<WebAppContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("WebAppContext") ?? throw new InvalidOperationException("Connection string 'WebAppContext' not found.")));
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
